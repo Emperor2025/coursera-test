c9ca748f2c701f2b8ca24f10a0bb3a7f1cb45391
Emperor2025 <riya.0225@gmail.com>
2017-02-28 14:35:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,17 +16,6 @@ company[stockPropName] = 110;
 console.log("Stock price is: " + 
   company[stockPropName]);
 
-// Better way: object literal
-var facebook = {
-  name: "Facebook",
-  ceo: {
-    firstName: "Mark",
-    favColor: "blue"
-  },
-  "stock of company": 110
-};
-
-console.log(facebook.ceo.firstName);
 // Functions are First-Class Data Types
 // Functions ARE objects
 function multiply(x, y) {
