22f794579c7c02c0bce53b1e2808fb228e1dfe00
Emperor2025 <riya.0225@gmail.com>
2017-03-21 14:57:37 +0530

    Create script0.js

---
+++ js/script0.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var message = 
+                res.name + " " 
+              if (res.name) {
+                message +=
+              }
+              else {
+                message +=
+              }
+              message += " and uses ";
+              message += res.numberOfDisplays + 1;
+              message += " displays for coding.";
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+      });
+  }
+);
+
+
+
+
+
