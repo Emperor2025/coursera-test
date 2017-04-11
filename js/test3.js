9c7bfb1e3f2eb2c4ccc83ddb0b03ce6916b811ca
Emperor2025 <riya.0225@gmail.com>
2017-04-11 15:30:14 +0530

    Update test3.js

--- js/test3.js
+++ js/test3.js
@@ -8,19 +8,13 @@ document.addEventListener("DOMContentLoaded",
         
         // Call server to get the name
         $ajaxUtils
-          .sendGetRequest("data/name.json", 
+          .sendGetRequest("data/m3.json", 
             function (res) {
               var message = 
                 res.firstName + " " + res.lastName
               if (res.likesChineseFood) {
                 message += " likes Chinese food";
               }
-              else {
-                message += " doesn't like Chinese food";
-              }
-              message += " and uses ";
-              message += res.numberOfDisplays + 1;
-              message += " displays for coding.";
 
               document.querySelector("#content")
                 .innerHTML = "<h2>" + message + "</h2>";
