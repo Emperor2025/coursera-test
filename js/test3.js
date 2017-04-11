516fda80c7e3bcdd3b871ef8e9a8ff9926a8a169
Emperor2025 <riya.0225@gmail.com>
2017-04-11 15:26:14 +0530

    Update test3.js

--- js/test3.js
+++ js/test3.js
@@ -1,49 +1,31 @@
-
 // Event handling
 document.addEventListener("DOMContentLoaded",
   function (event) {
-  
-     function sayHello (event) {
-      this.textContent = "Display";
-      var name =
-       document.getElementById("number").value;
-       var message = "<h2>Hello " + name + "!</h2>";
-
-      document
-        .getElementById("content")
-        .innerHTML = message;
-
+    
     // Unobtrusive event binding
     document.querySelector("button")
-      .addEventListener("click", sayHello);
-
-  }
-);
-
-
-
-// document.querySelector("button")
-//   .onclick = sayHello;
-
-    
-document.querySelector("button")
       .addEventListener("click", function () {
         
         // Call server to get the name
         $ajaxUtils
-          .sendGetRequest("data/m3.json", 
-            function (request) {
-              var number = request.responseText;
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var message = 
+                res.firstName + " " + res.lastName
+              if (res.likesChineseFood) {
+                message += " likes Chinese food";
+              }
+              else {
+                message += " doesn't like Chinese food";
+              }
+              message += " and uses ";
+              message += res.numberOfDisplays + 1;
+              message += " displays for coding.";
 
               document.querySelector("#content")
-                .innerHTML = "<h2>Hello " + name + "!</h2>";
+                .innerHTML = "<h2>" + message + "</h2>";
             });
-
-        
       });
   }
 );
 
-
-// document.querySelector("button")
-//   .onclick = sayHello;
