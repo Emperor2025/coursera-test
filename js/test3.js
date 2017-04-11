1973620b126949955061ceb4f0562f978cd6664c
Emperor2025 <riya.0225@gmail.com>
2017-04-11 15:05:58 +0530

    Update test3.js

--- js/test3.js
+++ js/test3.js
@@ -1,22 +1,38 @@
+
 // Event handling
 document.addEventListener("DOMContentLoaded",
   function (event) {
     
+    function sayHello (event) {
+      this.textContent = "Display";
+      var name =
+       document.getElementById("name").value;
+       var message = "<h2>Hello " + name + "!</h2>";
+
+      document
+        .getElementById("content")
+        .innerHTML = message;
+
+      if (name === "student") {
+        var title = 
+          document
+            .querySelector("#title")
+            .textContent;
+        title += " & Lovin' it!";
+        document
+            .querySelector("h1")
+            .textContent = title;
+      }
+    }
+
     // Unobtrusive event binding
     document.querySelector("button")
-      .addEventListener("click", function () {
-        
-        // Call server to get the name
-        $ajaxUtils
-          .sendGetRequest("data/m3.json", 
-            function (request) {
-              var name = request.responseText;
+      .addEventListener("click", sayHello);
 
-              document.querySelector("#content")
-                .innerHTML = "<h2>Hello " + name + "!</h2>";
-            });
-
-        
-      });
   }
 );
+
+
+
+// document.querySelector("button")
+//   .onclick = sayHello;
